fecdfebe373f19981fb296bc927ff070dfda12fe
angpace <108838753+angpace@users.noreply.github.com>
2023-02-11 16:42:12 -0500

    First commit, client side routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Home from './components/Home';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+          <br/>
+          <Link to="/blogs">Blogs</Link>
+        </ul>
+      </nav>  
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
     </div>
   );
 }
---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+function Home () {
+
+    return (
+        <div>
+            <h2>Hello from home!</h2>
+        </div>
+    )
+}
+
+export default Home;
