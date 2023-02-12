2ea806b627dab7d02df2d4fdd6b53895e6576ebf
angpace <108838753+angpace@users.noreply.github.com>
2023-02-11 19:05:51 -0500

    Building out database. Creating blog test. House and Quill Icons.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import Home from './components/Home';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
+import Blogs from './components/Blogs';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <h1>Kitchen and Code</h1> 
       <nav>
-        <ul>
-          <Link to="/">Home</Link>
-          <br/>
-          <Link to="/blogs">Blogs</Link>
+        <ul className='navlist'>
+          <li><Link to="/">
+            <img src="https://www.iconpacks.net/icons/1/free-home-icon-1235-thumb.png" alt="houseIcon" className='houseIcon'></img>
+            </Link></li>
+          <li><Link to="/blogs">
+            <img src="https://cdn-icons-png.flaticon.com/512/16/16294.png" alt="quill" className='quill'></img>
+            </Link></li>
         </ul>
       </nav>  
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/blogs" element={<Blogs/>} />
+        </Routes>
     </div>
   );
 }
