9f17ff1c4ac85cb1c1449afeedca24c094fd8272
angpace <108838753+angpace@users.noreply.github.com>
2023-02-13 16:03:59 -0500

    Adding react-scroll

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Blogs from './components/Blogs';
 
+
+
+
 function App() {
   return (
     <div>
       <h1>Kitchen and Code</h1> 
       <nav>
-        <ul className='navlist'>
-          <li><Link to="/">
-            <img src="https://www.iconpacks.net/icons/1/free-home-icon-1235-thumb.png" alt="houseIcon" className='houseIcon'></img>
-            </Link></li>
-          <li><Link to="/blogs">
-            <img src="https://cdn-icons-png.flaticon.com/512/16/16294.png" alt="quill" className='quill'></img>
-            </Link></li>
-        </ul>
-      </nav>  
+            <ul className='navlist'>
+              <li><Link to="/">
+                <img src="https://www.iconpacks.net/icons/1/free-home-icon-1235-thumb.png" alt="houseIcon" className='houseIcon'></img>
+                </Link></li>
+              <li><Link to="/blogs">
+                <img src="https://cdn-icons-png.flaticon.com/512/16/16294.png" alt="quill" className='quill'></img>
+                </Link></li>
+            </ul>
+          </nav>  
 
+     
+
+        {/* Routes */}
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/blogs" element={<Blogs/>} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,32 @@
 import About from "./About";
 import BlogPreview from "./BlogPreview";
+import { ScrollContainer, ScrollPage, Animator, Sticky, Zoom, batch, Fade, MoveOut, StickyIn, FadeIn, ZoomIn} from 'react-scroll-motion';
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
 
 function Home () {
 
     return (
         <div>
-            <h2>Hello from home!</h2>
-            <About />
-            <BlogPreview />
+            <ScrollContainer>
+            <ScrollPage >
+                <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                    <h2>Welcome to Kitchen & Code!</h2>
+                </Animator>
+          </ScrollPage>
+          <ScrollPage >
+                <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+                    <About/>
+                </Animator>
+          </ScrollPage>
+
+          <ScrollPage >
+                <Animator animation={ZoomInScrollOut}>
+                <BlogPreview/>
+                </Animator>
+           </ScrollPage>
+
+      </ScrollContainer>
         </div>
     )
 }
