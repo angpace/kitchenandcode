9bcef4ccb9c81b87e4d8baba8d8dc98de675881a
angpace <108838753+angpace@users.noreply.github.com>
2023-02-22 12:57:36 -0500

    Used same function for getting blog post on previews

--- client/src/components/PreviewCard.js
+++ client/src/components/PreviewCard.js
@@ -1,8 +1,15 @@
+import { useNavigate} from "react-router-dom";
+
 function PreviewCard ({b}) {
+    const navigate = useNavigate()
+
+    function getBlogPost (b) {
+        navigate(`/${b.id}`)
+    }
 
     return (
 
-                  <div className="title_card">
+                  <div className="title_card" onClick={() => getBlogPost(b)}>
                      <h3 className="preview_title"> {b.title}</h3>
                      <img alt="feature" src={b.feature}></img>
                      <p>{b.preview}</p>
