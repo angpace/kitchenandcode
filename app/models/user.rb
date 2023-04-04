e60c5f7877c4d78125d970384755fe7b26dd0d40
angpace <108838753+angpace@users.noreply.github.com>
2023-04-04 14:23:46 -0400

    Making sure no one else can edit/delete blog

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,35 @@ class User < ApplicationRecord
         from = Email.new(email: 'welcome.kitchenandcode@gmail.com')
         to = Email.new(email: "#{self.email}")
         subject = 'Welcome!'
-        content = Content.new(type: 'text/plain', value: "Hi, there #{self.name}! 
+        content = Content.new(type: 'text/plain', value: "Hi, there #{self.name},
+        
+I wanted to personally welcome you to Kitchen and Code! The journey from Pastry Chef to Software Engineer has been challenging, exciting and fufilling. I'm looking forward to sharing that journey with you! As a subscriber, you'll be the first to know about New Blog Posts.
 
-    I wanted to personally welcome you and express my gratitude for joining me. The journey from Pastry Chef to Software Engineer has been challenging, exciting and fufilling. I'm looking forward to sharing that journey with you! As a subscriber, you'll be the first to know about new blog posts, special offers, and exclusive content.    
-    Thank you once again, I look forward to getting to know you better!
+I look forward to getting to know you better! 
+Angela Pace
+Software Engineer | Kitchenandcode.com
+        ")
+        mail = Mail.new(from, subject, to, content)
+        
+        sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid_api)
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response.status_code
+        puts response.body
+        puts response.headers
 
-    Best, 
-    Angela Pace
-    Content Creater | Kitchenandcode.com
+    end
+
+    def new_blog (blog)
+        
+        from = Email.new(email: 'welcome.kitchenandcode@gmail.com')
+        to = Email.new(email: "#{self.email}")
+        subject = "#{blog.title} is up!"
+        content = Content.new(type: 'text/plain', value: "Hi, there #{self.name}!  
+        A new blog, #{blog.title}, is up. Check it out!
+
+        Best, 
+        Angela Pace
+        Content Creater | Kitchenandcode.com
         ")
         mail = Mail.new(from, subject, to, content)
         
