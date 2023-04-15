dd6447b7a000497e971e94c2da75315e6e950700
angpace <108838753+angpace@users.noreply.github.com>
2023-04-15 12:25:05 -0400

    Beginning to implement comments, trying with test first.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
     belongs_to :user
+    has_many :comments, dependent: :destroy
     
     validates :feature, :image_one, :paragraph_one, :preview, :date, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,8 @@
 puts "...Seeding"
 
 user = User.find_by(email: "angpace13@gmail.com")
-user.destroy
+blog = Blog.find_by(title: "Is Pangram?")
 
-angela = User.create(name: "Angela Pace", email: "angpace13@gmail.com", password: "kitchcode", image: "https://www.gannett-cdn.com/presto/2020/03/17/USAT/c0eff9ec-e0e4-42db-b308-f748933229ee-XXX_ThinkstockPhotos-200460053-001.jpg?crop=1170%2C658%2Cx292%2Cy120&width=1200")
-
+comment_test = Comment.create(user_id: user.id, blog_id: blog.id, content: "This is awesome! Thanks for the tips." )
 
 puts "Done seeding!"
