3cadcc62ac3f34547f9a256f36c57d232e724fef
angpace <108838753+angpace@users.noreply.github.com>
2023-06-25 12:01:46 -0400

    Creating footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FooterFlex, FooterContent, FooterLinks, Link, FooterTitle, SocialMediaWrap } from './FooterElements'
+
+const Footer = () => {
+  return (
+    <FooterFlex>
+        <FooterContent>
+            <FooterLinks>
+              <FooterTitle>Kitchen and Code</FooterTitle>
+                  <Link to="/">Home</Link>
+                  <Link to="/about">About</Link>
+                  <Link to="/blogposts">Blog</Link>
+            </FooterLinks>
+            <FooterLinks>
+              <FooterTitle>Account</FooterTitle>
+                  <Link to="/subscribe">Subscribe</Link>
+                  <Link to="/login">Login</Link>
+                </FooterLinks>
+                <SocialMediaWrap>
+                    <a  href="https://www.linkedin.com/in/angpace/"><i className="fa-brands fa-linkedin fa-lg"></i></a>
+                    <a  href="https://github.com/angpace"><i className="fa-brands fa-github fa-lg" ></i></a>
+                    <a  href="https://medium.com/@angpace13"><i className="fa-brands fa-medium fa-lg"></i></a>
+                    <a  href="https://www.instagram.com/heyitspastry/?hl=en"><i className="fa-brands fa-instagram fa-lg"></i></a>
+                </SocialMediaWrap>
+                
+        </FooterContent>
+    </FooterFlex>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Footer/FooterElements.js
@@ -0,0 +1,49 @@
+import { Link as LinkS } from "react-router-dom";
+import styled from "styled-components";
+
+export const FooterFlex = styled.div`
+border-top: thin solid;
+/* max-width: 1100px; */
+`
+
+export const FooterContent = styled.div`
+padding: 10%;
+display: flex;
+justify-content: space-around;
+background-color: #faf0e6;
+
+@media screen and (max-width: 820px){
+    flex-direction: column;
+}
+`
+export const FooterTitle = styled.p`
+font-weight: 600;
+padding-top: 10px;
+`
+
+export const FooterLinks = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const Link = styled(LinkS)`
+text-decoration: none;
+color: black;
+
+&:hover{
+    color: #991221e3;
+    transition: 0.3s ease-out
+}
+`
+export const SocialMediaWrap = styled.div`
+display: flex;
+gap: 8%;
+align-items: center;
+max-width: 1100px;
+
+@media screen and (max-width: 820px){
+    gap: 1%;
+}
+`
+
