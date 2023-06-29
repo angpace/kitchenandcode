4e3366d256a4433bd801d45650740983caefcbce
angpace <108838753+angpace@users.noreply.github.com>
2023-06-29 15:27:10 -0400

    Featured posts, front end works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import Bloglead from "../components/BlogLead/Bloglead";
 import Carousel from "../components/Carousel/Carousel";
 import Header from "../components/Header/Header";
@@ -6,12 +7,26 @@ import { useState,useEffect } from "react";
 
 function Home () {
     // const FadeUp = batch(Fade(), Sticky());
-  
+    const navigate = useNavigate()
+    const [featuredPosts, setFeaturedPosts] = useState([])
+
+  useEffect(() => {
+    fetch("/blogs")
+    .then(res => res.json())
+    .then(data => setFeaturedPosts(data))
+  }, [])
+
+  function getBlog(id){
+    if (id){
+        console.log(id)
+        navigate(`/${id}`)
+    }
+  }
 
     return (
        <div>
             <Header/>
-            <Carousel/>   
+            <Carousel featuredPosts={featuredPosts} getBlog={getBlog}/>   
             <Bloglead/>  
         </div>
         
