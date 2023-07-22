336c91fa9feb49c591f35858de3d16cdce45bfa0
angpace <108838753+angpace@users.noreply.github.com>
2023-07-22 01:03:33 -0400

    Fixing button

--- client/src/components/Recipes/RecipeElements.js
+++ client/src/components/Recipes/RecipeElements.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const RecipeContainer = styled.div`
+padding-top: 7%;
+
+@media screen and (max-width: 440px){
+    padding-top: 12%;
+}
+`
 export const MiniTitle = styled.h3`
 padding: 2%;
 `
