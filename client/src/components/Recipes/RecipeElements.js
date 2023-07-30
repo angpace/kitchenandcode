b0eac7779db05201e952b1dd0422fc5f751c24a4
angpace <108838753+angpace@users.noreply.github.com>
2023-07-30 18:53:55 -0400

    changing recipe image to circle

--- client/src/components/Recipes/RecipeElements.js
+++ client/src/components/Recipes/RecipeElements.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const RecipeContainer = styled.div`
-padding-top: 7%;
+padding-top: 10%;
 
 @media screen and (max-width: 440px){
-    padding-top: 12%;
+    padding-top: 15%;
 }
 `
 export const MiniTitle = styled.h3`
@@ -13,10 +13,19 @@ padding: 2%;
 
 export const RecipeImageContainer = styled.div`
 display: flex;
+flex-direction: column;
 justify-content: center;
+align-items: center;
 padding: 5%;
 `
 
 export const RecipeImage = styled.img`
-width: 70%;
+height: 200px;
+width: 200px;
+border-radius: 50%
+`
+export const RecipeTitle = styled.p`
+font-size: 22px;
+
+
 `
