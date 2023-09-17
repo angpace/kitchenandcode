5638d4da7101c8bed21c6e5a4e6a9cbd24926435
angpace <108838753+angpace@users.noreply.github.com>
2023-09-17 14:24:58 -0400

    Navbar updates

--- client/src/components/NewNav/NewNav.js
+++ client/src/components/NewNav/NewNav.js
@@ -1,9 +1,60 @@
 import React from 'react'
-import { NavbarContainer } from './NewNavElements.js'
+import { NavbarContainer, Logo, NavLink } from './NewNavElements.js'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import pom from '../NavBar/pom2.png';
 
 const NewNavBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+
   return (
-    <NavbarContainer>hi</NavbarContainer>
+    <NavbarContainer>
+      {['sm'].map((expand) => (
+         <Navbar
+         key={expand}
+         expand={expand}
+         expanded={expanded}
+       >
+        <Logo as={Link} to="/">Kitchen and Code</Logo>
+         <Navbar.Toggle
+              aria-controls={`offcanvasNavbar-expand-${expand}`}
+              onClick={() => setExpanded(!expanded)}
+            />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+              show={expanded}
+              onHide={() => setExpanded(false)}
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title className="logo fs-1" id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Kitchen and Code
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body >
+                <Nav className="navbar-nav ms-auto px-4">
+                  <NavLink as={Link} to="/blogs/food" >
+                    Food
+                  </NavLink>
+                  <NavLink as={Link} to="/blogs/tech" >
+                    Tech
+                  </NavLink>
+                  <NavLink as={Link} to="/subscribe">
+                    Subscribe
+                  </NavLink>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+            </Navbar>
+))}
+    </NavbarContainer>
   )
 }
 
--- client/src/components/NewNav/NewNavElements.js
+++ client/src/components/NewNav/NewNavElements.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const NavbarContainer = styled.div`
-padding: 2%;
+padding: 1%;
+`
+
+export const Logo = styled.h1`
+font-size: 50px;
+text-decoration: none;
+color: black;
+border-bottom: thin solid;
+`
+export const NavLink = styled.h3`
 `
