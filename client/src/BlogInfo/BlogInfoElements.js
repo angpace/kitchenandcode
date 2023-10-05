7d39e1d4962fee4ea363423ea2fc55707e4bd9bb
angpace <108838753+angpace@users.noreply.github.com>
2023-10-05 16:01:29 -0400

    deleting header, category and blog lead.

---
+++ client/src/BlogInfo/BlogInfoElements.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const BlogInfoContainer = styled.div`
+display: flex;
+max-width: 85%;
+margin: auto;
+
+@media screen and (max-width: 600px){
+    flex-direction: column
+}
+`
