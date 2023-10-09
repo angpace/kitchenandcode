69f3ccbb7036b238bee1963debe1cb3c05c6ac07
angpace <108838753+angpace@users.noreply.github.com>
2023-10-09 13:05:56 -0400

    Adding featured columns

---
+++ client/src/FeaturedColumn/FeaturedColumn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Fixed, Box } from './FeaturedColumnElements'
+
+const FeaturedColumn = () => {
+  return (
+    <Fixed>
+        <Box>Box 1</Box>
+        <Box>Box 2</Box>
+    </Fixed>
+  )
+}
+
+export default FeaturedColumn
