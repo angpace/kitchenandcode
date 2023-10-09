b3d770860fbe12b95133c62daf602803f4925b58
angpace <108838753+angpace@users.noreply.github.com>
2023-10-09 13:14:17 -0400

    Adding dummy content for Most recent, most liked.

--- client/src/FeaturedColumn/FeaturedColumnElements.js
+++ client/src/FeaturedColumn/FeaturedColumnElements.js
@@ -5,9 +5,13 @@ export const Fixed = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-between;
-width: 35%;
+width: 33%;
  position: sticky;
   top: 0;
+
+  @media screen and (max-width: 600px){
+    width: 100%;
+  }
 `
 
 export const Box = styled.div`
