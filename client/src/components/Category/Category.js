48810216a44373215a24d783585799b78c0ee147
angpace <108838753+angpace@users.noreply.github.com>
2023-10-17 12:08:01 -0400

    Adding styling to category

--- client/src/components/Category/Category.js
+++ client/src/components/Category/Category.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import BlogCard from "../../components/BlogCard/BlogCard";
+import { CategoryPage, CardHolder } from './CategoryElements';
 
 const Category = () => {
     const params = useParams()
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+      fetch('/blogs')
+      .then((res) => res.json())
+      .then((data) => setBlogs(data))
+    }, [])
+    
+   let categorized = blogs.filter((b) => b.category === params.id)
+
+   let cards = categorized.map((b) => {
+    return <BlogCard b={b}/> })
 
     if(params.id === "tech"){
         return (
-            <>
-            <h1>Technology</h1>
-            </>
+            <CategoryPage>
+            <h1>Blogs about Technology</h1>
+              <CardHolder>
+                {cards}
+                </CardHolder>
+            </CategoryPage>
             )
     }
   return (
-    <h1>Food</h1>
+    <CategoryPage>
+        <h1>Blogs about Food</h1>
+          <CardHolder>
+          {cards}
+          </CardHolder>
+    </CategoryPage>
   )
 }
 
---
+++ client/src/components/Category/CategoryElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CategoryPage = styled.div`
+width: 80%;
+margin: auto;
+margin-top: 40px;
+`
+
+export const CardHolder = styled.div`
+padding-top: 20px;
+max-width:60%;
+
+`
